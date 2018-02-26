4436c0cea258dd435b328b64992ab9e38c20c68e
Pawel Guz <pguz@fxcm.com>
2018-02-26 12:56:38 +0000

    counter the javascipt floating point arithmetic inaccuracy when printing prices

--- liveprices.js
+++ liveprices.js
@@ -27,6 +27,11 @@ var setupCLI = () => {
 var priceUpdate = (update) => {
 	try {
 		var jsonData = JSON.parse(update);
+		// JavaScript floating point arithmetic is not accurate, so we need to round rates to 5 digits
+		// Be aware that .toFixed returns a String
+		jsonData.Rates = jsonData.Rates.map(function(element){
+			return element.toFixed(5);
+		});
 		console.log(`@${jsonData.Updated} Price update of [${jsonData.Symbol}]: ${jsonData.Rates}`);
 	} catch (e) {
 		console.log('price update JSON parse error: ', e);
